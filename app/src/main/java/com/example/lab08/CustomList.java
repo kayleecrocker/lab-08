6b11b42ed8b1a0a589e1de479fe1d9537d224574
kayleecrocker <kayleevc9@gmail.com>
2026-03-05 21:21:34 -0700

    test passed

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -17,5 +17,7 @@ public class CustomList {
     
     // i am adding a comment
     // Will be implemented later using TDD (leave blank for now)
-    // public boolean hasCity(City city) { ... }
+    public boolean hasCity(City city) {
+        return cities.contains(city);
+    }
 }
